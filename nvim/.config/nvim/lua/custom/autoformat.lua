e017809551d7be8552b283a2c44975be99aa5cfc
Ritvars Zvejnieks <ritvars.zvejnieks@applyit.lv>
2025-02-20 14:10:59 +0200

    So my boss wanted this button ...

--- nvim/.config/nvim/lua/custom/autoformat.lua
+++ nvim/.config/nvim/lua/custom/autoformat.lua
@@ -2,10 +2,17 @@ local setup = function()
   -- Autoformatting Setup
   local conform = require "conform"
   conform.setup {
+    quiet = true,
     formatters_by_ft = {
       lua = { "stylua" },
       php = { "pint" },
+      -- if vim.bo.filetype == 'javascript' or vim.bo.filetype == 'typescript' or vim.bo.filetype == 'vue' then
+      -- disable formtting because eslint.
+      javascript = nil,
+      typescript = nil,
+      vue = nil,
     },
+
     format_on_save = {
       lsp_format = "fallback",
       timeout_ms = 500,

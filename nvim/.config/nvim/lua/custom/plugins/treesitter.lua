9af9570d96f6d40496ce2553fe33b87e8d70bb8f
Ritvars Zvejnieks <ritvars.zvejnieks@applyit.lv>
2026-04-20 14:51:58 +0300

    Lock S-foils in attack position

--- nvim/.config/nvim/lua/custom/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -8,6 +8,15 @@ return {
       require('nvim-treesitter').install {
         "javascript", "typescript", "php", "lua", "rust", "html", "vue", "jsdoc", "c", "vimdoc", "bash"
       }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.wo[0][0].foldmethod = 'expr'
+          -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end,
   },
 }
